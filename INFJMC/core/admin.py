50790908921e03a29234be067f4b76b983654057
AlumnosInf <AlumnosInf@soportetiu.local>
2023-05-12 11:51:49 -0400

    Ultima actualizacion

--- INFJMC/core/admin.py
+++ INFJMC/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from core.models import carrera,docente             #Se importan el modelo
 
 # Register your models here.
+
+class CarreraAdmin(admin.ModelAdmin):
+    pass
+
+class DocenteAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(carrera,CarreraAdmin)   # Permite gestionar la tabla carreras
+admin.site.register(docente,DocenteAdmin)
